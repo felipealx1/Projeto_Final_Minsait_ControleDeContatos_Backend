df82e87751d86bc6bd5093c9befdc78f2cc2fca9
Jose Felipe <felipealexandre178@gmail.com>
2023-10-31 11:40:07 -0300

    Primeiro commit

---
+++ ApiControleDeContatos/src/main/java/com/ControleDeContatos/ApiControleDeContatos/ApiControleDeContatosApplication.java
@@ -0,0 +1,13 @@
+package com.ControleDeContatos.ApiControleDeContatos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiControleDeContatosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiControleDeContatosApplication.class, args);
+	}
+
+}
---
+++ ApiControleDeContatos/src/test/java/com/ControleDeContatos/ApiControleDeContatos/ApiControleDeContatosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ControleDeContatos.ApiControleDeContatos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiControleDeContatosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
